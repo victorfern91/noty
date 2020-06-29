c0028ebe67fc816fb1dcf0f32c5664c843c82470
Victor Fernandes <victor.fernandes@talkdesk.com>
2020-06-29 22:06:16 +0100

    First draft of noty class

---
+++ noty.js
@@ -0,0 +1,44 @@
+class Noty {
+  constructor() {
+    this._listeners = new Map();
+  }
+
+  _getEventHandlers(eventName) {
+    if (this._listeners.has(eventName)) {
+      return this._listeners.get(eventName);
+    }
+
+    return [];
+  }
+
+  _getAllEventsHandlers(eventName) {
+    return [...this._getEventHandlers(eventName), ...this._getEventHandlers('*')];
+  }
+
+  on(eventName, callback) {
+    this._listeners.set(eventName, [...this._getEventHandlers(eventName), callback]);
+  }
+
+  emit(eventName, args = null) {
+    this._getAllEventsHandlers(eventName).forEach(eventCallback => eventCallback(args));
+  }
+
+  off(eventName) {
+    this._listeners.delete(eventName);
+  }
+
+  removeListener(eventName, callback) {
+    const eventHandlers = this._getEventHandlers(eventName).filter(eventHandler => eventHandler !== callback);
+
+    this._listeners.set(eventName, eventHandlers)
+  }
+}
+
+const noty_singleton = new Noty();
+
+export default {
+  on: noty_singleton.on,
+  emit: noty_singleton.emit,
+  off: noty_singleton.off,
+  removeListener: noty_singleton.removeListener
+};
