a3b74f55fcf3cc471aebb577f748d4f5a1e44f21
Victor Fernandes <victor.fernandes@talkdesk.com>
2020-06-30 08:59:17 +0100

    reset mocks on tests

--- __tests__/noty.spec.js
+++ __tests__/noty.spec.js
@@ -1,33 +1,34 @@
 import noty from '../noty';
 
 describe('noty', () => {
-  let listener;
+  const listener = jest.fn();
 
   beforeEach(() => {
-    listener = jest.fn();
+    jest.resetAllMocks();
   });
 
-  it("should execute a callback when the event registered is triggered", () => {
+  afterEach(() => {
+    // clears all the event listeners used in tests
+    [...noty._listeners.keys()].map(eventName => noty.off(eventName));
+  });
+
+  it('ould execute a callback when the event registered is triggered', () => {
     noty.on('sample-name', listener);
 
     noty.emit('sample-name');
 
     expect(listener).toHaveBeenCalledTimes(1);
-
-    noty.off('sample-name');
   });
 
-  it("should not execute a callback when the event registered is not triggered", () => {
+  it('should not execute a callback when the event registered is not triggered', () => {
     noty.on('sample-name', listener);
 
     noty.emit('sample-name-not');
 
     expect(listener).not.toHaveBeenCalled();
-
-    noty.off('sample-name');
   });
 
-  it("should execute a callback on all events if a wildcard is used", () => {
+  it('should execute a callback on all events if a wildcard is used', () => {
     noty.on('*', listener);
 
     noty.emit('sample-name');
@@ -36,7 +37,5 @@ describe('noty', () => {
     noty.emit('sample-name-3');
 
     expect(listener).toHaveBeenCalledTimes(4);
-
-    noty.off('sample-name');
   });
 });
